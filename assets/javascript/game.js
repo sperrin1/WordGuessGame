ce5f9165640341b26838b58105c4b862aa33b5f2
Reszka <reszka2@gmail.com>
2018-05-19 10:22:25 -0700

    hangman v1.01

---
+++ assets/javascript/game.js
@@ -0,0 +1,48 @@
+    //words to guess
+var words = [
+    "staircase",
+    "secular",
+    "circulation",
+    "opinion",
+    "torture",
+    "stomach",
+    "beautiful",
+    "displace",
+    "specimen",
+    "function"
+   ];
+   
+   //randonmly picks a word from the list
+var word = words[Math.floor(Math.random() * words.length)];
+   
+   // creates "blank" spaces matching the word length
+var answerArray = [];
+for (var i = 0; i < word.length; i++) {
+ answerArray[i] = "_";
+}
+var remainingLetters = word.length;
+
+
+    while (remainingLetters > 0) {
+        // Show the player their progress
+        alert(answerArray.join(" "));
+        // Get a guess from the player
+        if (guess === null) {
+            // Exit the game loop
+            break;
+        } else if (guess.length !== 1) {
+            alert("Please enter a single letter.");
+        } else {
+            // Update the game state with the guess
+            for (var j = 0; j < word.length; j++) {
+                if (word[j] === guess) {
+                    answerArray[j] = guess;
+                    remainingLetters--;
+                }
+            }
+        }
+        // The end of the game loop
+    }
+
+alert(answerArray.join(" "));
+alert("Good job! The answer was " + word);
